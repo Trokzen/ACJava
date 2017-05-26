39afb03643c3d6d9e33f14a821676642e7c7c267
Robert <Nadzar@yandex.ru>
2017-05-26 11:57:07 +0300

    Update AhoCorasickTest.java

--- src/test/java/projectBFS/AhoCorasickTest.java
+++ src/test/java/projectBFS/AhoCorasickTest.java
@@ -1,29 +1,25 @@
 package projectBFS;
-
+import java.util.*;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.*;
 
 /**
  * Created by Trokzen on 25.05.2017.
  */
- public class AhoCorasickTest extends AhoCorasick2.Node
+ public class AhoCorasickTest extends AhoCorasick.Node
 {
     @Test//Тестируем несколько подстрок
-    public void testEquals2() {
-        AhoCorasick2 ahoCorasick2 = new AhoCorasick2(1000);
-        ahoCorasick2.addString("bac");
-        ahoCorasick2.addString("a");
+    public void testEquals() {
+        AhoCorasick ahoCorasick = new AhoCorasick(1000);
+        ahoCorasick.addString("bac");
+        ahoCorasick.addString("a");
         String s = "abacbaca";
         int node = 0;
         List<Integer> positions = new ArrayList<>();
         for (int i = 0; i < s.length(); i++) {
-            node = ahoCorasick2.transition(node, s.charAt(i));
-            if (ahoCorasick2.nodes[node].leaf)
+            node = ahoCorasick.transition(node, s.charAt(i));
+            if (ahoCorasick.nodes[node].leaf)
                 positions.add(i);
         }
         List<Integer> positionsTest = new ArrayList<>();
@@ -34,16 +30,16 @@ import static org.junit.Assert.assertNotEquals;
         assertEquals(positionsTest,positions);
     }
     @Test//Тестируем с инъекцией ошибки
-    public void testNotEquals2() {
-        AhoCorasick2 ahoCorasick2 = new AhoCorasick2(1000);
-        ahoCorasick2.addString("bac");
-        ahoCorasick2.addString("a");
+    public void testNotEquals() {
+        AhoCorasick ahoCorasick = new AhoCorasick(1000);
+        ahoCorasick.addString("bac");
+        ahoCorasick.addString("a");
         String s = "abacbaca";
         int node = 0;
         List<Integer> positions = new ArrayList<>();
         for (int i = 0; i < s.length(); i++) {
-            node = ahoCorasick2.transition(node, s.charAt(i));
-            if (ahoCorasick2.nodes[node].leaf)
+            node = ahoCorasick.transition(node, s.charAt(i));
+            if (ahoCorasick.nodes[node].leaf)
                 positions.add(i);
         }
         List<Integer> positionsTest = new ArrayList<>();
@@ -54,58 +50,20 @@ import static org.junit.Assert.assertNotEquals;
         assertNotEquals(positionsTest,positions);
     }
     @Test//Тестируем с инъекцией ошибки
-    public void testOneStr2() {
-        AhoCorasick2 ahoCorasick2 = new AhoCorasick2(1000);
-        ahoCorasick2.addString("bac");
-        String s = "abacbaca";
-        int node = 0;
-        List<Integer> positions = new ArrayList<>();
-        for (int i = 0; i < s.length(); i++) {
-            node = ahoCorasick2.transition(node, s.charAt(i));
-            if (ahoCorasick2.nodes[node].leaf)
-                positions.add(i);
-        }
-        List<Integer> positionsTest = new ArrayList<>();
-        positionsTest.add(3);
-        positionsTest.add(6);
-        assertEquals(positionsTest,positions);
-    }
-    @Test//Другой случай поиска строк
     public void testOneStr() {
-        AhoCorasick2 ahoCorasick2 = new AhoCorasick2(1000);
-        ahoCorasick2.addString("a");
-        ahoCorasick2.addString("b");
+        AhoCorasick ahoCorasick = new AhoCorasick(1000);
+        ahoCorasick.addString("bac");
         String s = "abacbaca";
         int node = 0;
         List<Integer> positions = new ArrayList<>();
         for (int i = 0; i < s.length(); i++) {
-            node = ahoCorasick2.transition(node, s.charAt(i));
-            if (ahoCorasick2.nodes[node].leaf)
-                positions.add(i);
-        }
-        List<Integer> positionsTest = new ArrayList<>();
-        positionsTest.add(0);
-        positionsTest.add(1);
-        positionsTest.add(2);
-        positionsTest.add(4);
-        positionsTest.add(5);
-        positionsTest.add(7);
-        assertEquals(positionsTest,positions);
-    }
-    @Test(timeout = 1000)//Тест с ограничение времени на прохождение
-    public void time() {
-        AhoCorasick2 ahoCorasick2 = new AhoCorasick2(1000);
-        ahoCorasick2.addString("bac");
-        String s = "abacbaca";
-        int node = 0;
-        List<Integer> positions = new ArrayList<>();
-        for (int i = 0; i < s.length(); i++) {
-            node = ahoCorasick2.transition(node, s.charAt(i));
-            if (ahoCorasick2.nodes[node].leaf)
+            node = ahoCorasick.transition(node, s.charAt(i));
+            if (ahoCorasick.nodes[node].leaf)
                 positions.add(i);
         }
         List<Integer> positionsTest = new ArrayList<>();
         positionsTest.add(3);
         positionsTest.add(6);
+        assertEquals(positionsTest,positions);
     }
 }
